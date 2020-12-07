b773e78c2c1d1a90238795c16c52c41d6b736f82
edvinas10 <71633786+edvinas10@users.noreply.github.com>
2020-12-07 23:56:16 +0200

    Add files via upload

---
+++ class1_2.h
@@ -0,0 +1,59 @@
+#ifndef CLASS1_H_INCLUDED
+#define CLASS1_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <cstdlib> //Del rand() funkcijos
+#include <time.h>  //del "time"
+#include <ctime>
+#include <fstream>
+#include <random>
+#include <iomanip> // del setprecision
+#include <chrono>
+
+using namespace std;
+using std::cout;
+using std::cin;
+using std::string;
+using std::swap;
+
+
+class duomuo {
+  public:
+  string Vard;
+  string Pav;
+  std::vector<int> paz;
+  int egz;
+  double GP=0;
+
+public:
+  duomuo() : egz(0) { }
+  ~duomuo(){cout<<"de";};
+  duomuo(std::istream& is);
+  
+  duomuo(const duomuo &duomuo_kopijuoti) {Vard = duomuo_kopijuoti.Vard; Pav = duomuo_kopijuoti.Pav; paz = duomuo_kopijuoti.paz; egz = duomuo_kopijuoti.egz; GP=duomuo_kopijuoti.GP; }  
+    duomuo& operator=(const duomuo& rhs)
+   {
+      Vard = rhs.Vard;
+      Pav = rhs.Pav;
+      paz = rhs.paz;
+      egz = rhs.egz;
+      GP = rhs.GP;
+      return *this;
+   }
+  inline std::string vardas() const { return Vard; }    // get'eriai, inline
+  inline std::string pavarde() const { return Pav; }  // get'eriai, inline
+  double GBal(double (*) (vector<double>)) const;  // get'eriai
+  std::istream& readStudent(std::istream&);  // set'eriai
+ 
+};
+  //duomuo::~duomuo(){cout<<"del";}; 
+void rikiuoti(std::vector<int> masyvas, int n);
+
+double rastiMediana(std::vector<int> masyvas, int n);
+
+int randomSk();
+
+#endif // CLASS1_H_INCLUDED
