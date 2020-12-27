7d7b36b31fcf2529a6dd8434822b4216aaeecc60
edvinas10 <71633786+edvinas10@users.noreply.github.com>
2020-12-27 15:26:46 +0200

    Add files via upload

---
+++ class1_5.cpp
@@ -0,0 +1,26 @@
+#include "class1_5.h"
+
+//rikiuoti(Eil.paz);
+//rikiuoti(Eil.egz);
+void rikiuoti(std::vector<int> masyvas, int n)  
+{  
+    int i, j;  
+    for (i = 0; i < n-1; i++)      
+      for (j = i+1; j < n; j++)  
+        if (masyvas[i] > masyvas[j])  
+          std::swap(masyvas[j], masyvas[i]);  
+}
+double rastiMediana(std::vector<int> masyvas, int n) 
+{ 
+  rikiuoti (masyvas, n);
+    if (n % 2 != 0) 
+        return (double)masyvas[n / 2]; 
+    return (double)(masyvas[(n - 1) / 2] + masyvas[n / 2]) / 2.0; 
+}
+int randomSk() {
+    std::random_device rd;//random seed
+    std::mt19937 gen(rd());//random number generator
+    std::uniform_int_distribution<> dis(1, 10);//range
+    return dis(gen);//put generator in range
+    //srand((unsigned)time(0)); //susigeneruoju seed'as
+}
